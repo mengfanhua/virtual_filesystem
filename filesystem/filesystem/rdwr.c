0c2b6227f2c2282d4052bf0eec4c64fe40b337df
mengfanhua <1070985939@qq.com>
2020-06-14 00:46:25 +0800

    “磁盘”的读写

---
+++ filesystem/filesystem/rdwr.c
@@ -0,0 +1,25 @@
+#include "virtual_system.h"
+#include<malloc.h>
+#include<string.h>
+
+FILE *fp;
+
+char* read(int dnode_index) {
+	//读出一块数据区
+	char *p;
+	p = (char *)malloc(sizeof(char)*BLOCK_SIZE);
+	fseek(fp, DATA_START + dnode_index*BLOCK_SIZE, SEEK_SET);
+	fread(p, 1, BLOCK_SIZE, fp);
+	return p;
+}
+
+void write(int dnode_index, char* content) {
+	//写入一块数据区
+	fseek(fp, DATA_START + dnode_index*BLOCK_SIZE, SEEK_SET);
+	if (strlen(content) >= BLOCK_SIZE) {
+		fwrite(content, 1, BLOCK_SIZE, fp);
+	}
+	else {
+		fwrite(content, 1, strlen(content), fp);
+	}
+}
