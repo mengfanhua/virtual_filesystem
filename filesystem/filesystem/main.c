a1f752ac3e68c62ae1089a35b08d39972eb5f3b1
mengfanhua <1070985939@qq.com>
2020-06-14 20:34:08 +0800

    尚未实现，当前仅用来测试

--- filesystem/filesystem/main.c
+++ filesystem/filesystem/main.c
@@ -1,7 +1,11 @@
 #include "virtual_system.h"
+#include<string.h>
 
 int main() {
+	/*
 	printf("%zd\n",sizeof(struct superblock));
-	printf("%d", format());
+	printf("%d", format());*/
+	char *a = "1";
+	printf("%zd",strlen(a));
 	getchar();
 }
