ad7b68c947e39973268e5de17969781eb695dbcb
mengfanhua <1070985939@qq.com>
2020-06-19 00:35:11 +0800

    完成了命令行界面逻辑

--- surfaces/input_ui.cpp
+++ surfaces/input_ui.cpp
@@ -5,34 +5,38 @@
 #include <QLayoutItem>
 #include "virtual_system.h"
 #include <malloc.h>
-
-struct cur_path curpath;
-int new_index;
+#include "interfaces.h"
 
 input_ui::input_ui(QWidget *parent)
     : QWidget(parent)
 {
+	//禁用关闭键，使得可以自己设置关闭方法
     this->setWindowFlags(Qt::CustomizeWindowHint | Qt::WindowMinimizeButtonHint | Qt::WindowMaximizeButtonHint);
     this->setFixedSize(700, 700);
+	//主界面布局，如果需要添加button就在此更改
     one =new QGridLayout;
     this->setLayout(one);
 
+	//设置滚动条区，用于动态显示下一个命令提示符输入区
     bottom = new QScrollArea;
+	//滚动条区需要内置Widget
     bottomContent = new QWidget;
     contentLayout = new QVBoxLayout;
     bottom->setWidget(bottomContent);
-    bottom->setWidgetResizable(true);
+    bottom->setWidgetResizable(true);//设置内部Widget可拉伸，否则动态修改将失效
     bottomContent->setLayout(contentLayout);
-    contentLayout->setAlignment(Qt::AlignTop);
+    contentLayout->setAlignment(Qt::AlignTop);//设置纵向布局的起点是上部，由上至下拓展
 
-    one->addWidget(bottom,0,0);
+    one->addWidget(bottom,0,0);//后面的参数是行，列
 
-    path = new QLabel;
-    input = new QLineEdit;
+	//此处的代码其实可以省略，然后在该界面被触发时，利用槽函数初始化
+    path = new QLabel;//用于显示当前路径
+    input = new QLineEdit;//用于输入命令
     input->setFixedWidth(650);
     contentLayout->addWidget(path);
     contentLayout->addWidget(input);
-    connect(input,SIGNAL(returnPressed()),this,SLOT(solve_input()));
+    connect(input,SIGNAL(returnPressed()),this,SLOT(solve_input()));//输入区按下回车触发槽函数
+	//滚动条自动向下槽函数
     connect(bottom->verticalScrollBar(), &QAbstractSlider::rangeChanged, this, &input_ui::slider);
     showResult = NULL;
 }
@@ -41,208 +45,36 @@ input_ui::~input_ui()
 {
 }
 
-int _check_access(QString str){
-    int flag = 1;
-    int i;
-    for(i=0;i<str.size();i++){
-        char s = str.at(i).toLatin1();
-        if((s>=48&&s<=57)||(s>=65&&s<=90)||(s>=97&&s<=122)||s==95||s==46){
-            continue;
-        }
-        else{
-            flag=0;
-            break;
-        }
-    }
-    return flag;
-}
-
-int _mkdir(QStringList& param){
-    int flag = 0;
-    QStringList path = param.at(1).split("/");
-    int i;
-	for (i = 0; i < path.size(); i++) {
-		if (path.at(i).size <= MAX_DIR_NAME_SIZE) {
-			continue;
-		}
-		else {
-			break;
-		}
-	}
-	if (i == path.size()) {
-		for (i = 0; i<path.size(); i++) {
-			if (_check_access(path.at(i)) == 0) {
-				break;
-			}
-			else if (path.at(i) == "." || path.at(i) == "..") {
-				break;
-			}
-			else {
-				continue;
-			}
-		}
-		if (i == path.size()) {
-			int m = curpath.front->inum;
-			int i;
-			for (i = 0; i < path.size(); i++) {
-				int d = mkdir(m, path.at(i).toUtf8().data());
-				if (d == -1 || d == 0) {
-					flag = d - 1;
-					break;
-				}
-				m = new_index;
-			}
-			if (i == path.size) {
-				flag = 1;
-			}
-		}
-	}
-	else {
-		flag == -3;
-	}
-    return flag;
-}
-
-int _chdir(QStringList& param) {
-	int flag = 0;
-	QStringList path = param.at(1).split("/");
-	int i;
-	for (i = 0; i<path.size(); i++) {
-		if (_check_access(path.at(i)) == 0) {
-			break;
-		}
-		else if ((path.at(i) == "." && i != 0) || (path.at(i) == ".." && i != 0)) {
-			break;
-		}
-		else {
-			continue;
-		}
-	}
-	if (i == path.size()) {
-		int m;
-		if (path.at(0) == ".") {
-			m = curpath.front->inum;
-		}
-		else if (path.at(0) == "..") {
-			m = curpath.front->front->inum;
-		}
-		else {
-			m = exist_dir(curpath.front->inum, path.at(0).toUtf8().data());
-		}
-		int i,n = m;
-		for (i = 1; i < path.size(); i++) {
-			if (n != -1) {
-				n = exist_dir(n, path.at(i).toUtf8().data());
-			}
-			else {
-				break;
-			}
-		}
-		if (i == path.size()) {
-			struct cur_path *p;
-			if (path.at(0) == "..") {
-				p = curpath.front;
-				if (p != &curpath) {
-					p->front->next = p->next;
-					curpath.front = p->front;
-					free(p);
-				}
-			}
-			else if (path.at(0) != ".") {
-				p = (struct cur_path *)malloc(sizeof(struct cur_path));
-				p->inum = m;
-				p->front = curpath.front;
-				curpath.front->next = p;
-				p->next = &curpath;
-				curpath.front = p;
-			}
-			for (i = 1; i < path.size(); i++) {
-				p = (struct cur_path *)malloc(sizeof(struct cur_path));
-				m = exist_dir(m, path.at(i).toUtf8().data());
-				p->inum = m;
-				p->front = curpath.front;
-				curpath.front->next = p;
-				p->next = &curpath;
-				curpath.front = p;
-			}
-			flag = 1;
-			p = NULL;
-		}
-		else {
-			flag = -1;
-		}
-	}
-	return flag;
-}
-
-QString _dir() {
-	QString temp = "file         | type\n-------------------\n";
-	QString type1 = "| file";
-	QString type2 = "| dir";
-	QString type3 = "| fast";
-	struct inode *p;
-	p = iget(curpath.front->inum);
-	int i = 0, j = 0, k;
-	while (i < p->disk_block.filesize) {
-		if (p->disk_block.block_index[j] != MAX_FILE_NUM) {
-			QString te = QString(QLatin1String(dir[p->disk_block.block_index[j]].name));
-			for (k = te.size(); k < 13; k++) {
-				te = te + " ";
-			}
-			temp = temp + te;
-			switch (p->disk_block.file_type[j]) {
-			case 0:temp = temp + type1 + "\n"; break;
-			case 1:temp = temp + type2 + "\n"; break;
-			case 3:temp = temp + type3 + "\n"; break;
-			default:break;
-			}
-			i += 1;
-		}
-		j += 1;
-	}
-	temp = temp + "\n";
-	p = NULL;
-	return temp;
-}
-
-int _create(QStringList& param) {
-	int flag = 0;
-	if (param.at(1).size > MAX_DIR_NAME_SIZE) {
-		flag = -3;
-	}
-	else if (_check_access(param.at(1)) == 0) {
-		flag = -2;
-	}
-	else {
-		flag = create(curpath.front->inum, param.at(1).toUtf8().data());
-	}
-	return flag;
-}
-
-void input_ui::solve_input(){
-    QString instr = input->text();
-    QStringList param = instr.split(" ");
-    int i=param.size()-1;
+void input_ui::solve_input(){//换行触发的槽函数
+    QString instr = input->text();//获取当前内容
+    QStringList param = instr.split(" ");//空格划分
+    int i=param.size()-1;//此处注意，要倒序删除空串，以免出现越界情况，且i为int型，不要用对应的size_t类型，无负数会越界
     for(;i>=0;i--){
         if(param.at(i) == ""){
+			//循环去除空串
             param.removeAt(i);
         }
     }
-    QString response = "";
-    if(param.size() == 0){ }
+    QString response = "";//用于显示结果
+    if(param.size() == 0){ }//用户仅输入了回车或仅有空格
     else if(param.at(0) == "login" && param.size() == 1){
+		//登录
         emit log(0);
     }
     else if(param.at(0) == "logout" && param.size() == 1){
+		//登出，会关闭所有该用户打开的文件
         emit log(0);
     }
     else if(param.at(0) == "signup" && param.size() == 1){
+		//注册
         emit log(1);
     }
     else if(param.at(0) == "chuser" && param.size() == 1){
+		//切换账户，不会关闭当前用户打开的文件，以此实现同步共享
         emit log(0);
     }
     else if(param.at(0) == "mkdir" && param.size() == 2){
+		//创建文件夹，或多层文件夹，但不能使用/, ./, ../的方式，只能为: 新文件夹名/
         int flag = _mkdir(param);
 		if (flag == 0) {
 			response = "ERROR: The path contains illegal characters.\n\n";
@@ -261,6 +93,7 @@ void input_ui::solve_input(){
 		}
     }
     else if(param.at(0) == "chdir" && param.size() == 2){
+		//切换路径，可多级目录，支持 ./   ../  .  ..  文件夹名/    文件夹
 		int flag = _chdir(param);
 		if (flag == 0) {
 			response = "ERROR: The path contains illegal characters.\n\n";
@@ -294,13 +127,42 @@ void input_ui::solve_input(){
 		}
     }
     else if(param.at(0) == "open" && param.size() == 2){
-
+		int flag = _open(param);
+		if (flag == -1) {
+			response = "ERROR: Incorrect file type.\n\n";
+		}
+		else if (flag == 0) {
+			response = "ERROR: File does not exist.\n\n";
+		}
+		else {
+			response = "Done.\n\n";
+			this->hide();
+			emit fileopen(flag);
+		}
     }
     else if(param.at(0) == "delete" && param.size() == 2){
-
+		int flag = _delete(param);
+		if (flag == -1) {
+			response = "ERROR: Incorrect file type.\n\n";
+		}
+		else if (flag == 0) {
+			response = "ERROR: No operation authority or file does not exist.\n\n";
+		}
+		else {
+			response = "Done.\n\n";
+		}
     }
     else if(param.at(0) == "rm" && param.size() == 2){
-
+		int flag = _rm(param);
+		if (flag == -1) {
+			response = "ERROR: Incorrect dir type.\n\n";
+		}
+		else if (flag == 0) {
+			response = "DONE.\nWARNING: Some files are kept without permission or dir does not exist.\n\n";
+		}
+		else {
+			response = "Done.\n\n";
+		}
     }
     else if(param.at(0) == "exit" && param.size() == 1){
         this->close();
@@ -317,7 +179,22 @@ void input_ui::solve_input(){
 		 response = "Done.\n\n";
     }
 	else if (param.at(0) == "ls" && param.size() == 3) {
-
+		int flag = _ls(param);
+		if (flag == -1) {
+			response = "ERROR: Incorrect file type.\n\n";
+		}
+		else if (flag == 0) {
+			response = "ERROR: file does not exist.\n\n";
+		}
+		else if (flag == 1) {
+			response = "ERROR: Insufficient number of inodes.\n\n";
+		}
+		else if (flag == 2) {
+			response = "ERROR: Closed loop.\n\n";
+		}
+		else {
+			response = "Done.\n\n";
+		}
 	}
     else{
         response = "ERROR: Unrecognized instructions.\n\n";
