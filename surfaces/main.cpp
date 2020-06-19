fabb51ecedc74164a4232a5d0ba055ef72c88c8a
mengfanhua <1070985939@qq.com>
2020-06-19 16:12:29 +0800

    主函数，含有部分界面连接槽

--- surfaces/main.cpp
+++ surfaces/main.cpp
@@ -1,11 +1,24 @@
 #include "input_ui.h"
+#include "file_open.h"
+#include "interfaces.h"
+#include "logi.h"
 
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
+    if(_init() == 0){
+        return 0;
+    }
     QApplication a(argc, argv);
     input_ui w;
-    w.show();
+    file_open e;
+    logi p;
+    QObject::connect(&w,SIGNAL(fileopen(int)), &e, SLOT(openfile(int)));
+    QObject::connect(&e,SIGNAL(instruct()), &w, SLOT(back()));
+    QObject::connect(&p,SIGNAL(instr()), &w, SLOT(comein()));
+    QObject::connect(&p,SIGNAL(instr_s()), &w, SLOT(back()));
+    QObject::connect(&w,SIGNAL(log(int)), &p, SLOT(rec(int)));
+    p.show();
     return a.exec();
 }
